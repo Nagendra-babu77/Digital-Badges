afd9da519ac5ae457c2b9deb115cd3058c8c5d87
unknown <nb339777@gmail.com>
2022-02-26 19:58:13 +0530

    Added Home Page and Badge Details Upload page

--- Digital_Badges/App/models.py
+++ Digital_Badges/App/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Badge_Details(models.Model):
+    Name = models.CharField(max_length=100)  
+    Description=models.TextField()
+    Image=models.ImageField(upload_to='images/')
+    Eligible_students=models.JSONField()
