82b0ec1aabf181eabb797ce84df1683674cbf3a4
unknown <nb339777@gmail.com>
2022-02-27 13:00:07 +0530

    Added List of all the badges page with CRUD Operations

--- Digital_Badges/App/urls.py
+++ Digital_Badges/App/urls.py
@@ -5,5 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',index,name="index"),
-    path('UploadBadge/',upload_badge,name="UploadBadge") 
+    path('UploadBadge/',upload_badge,name="UploadBadge"),
+    path('ListofBadges/',badges_list,name="badges_list"), 
+    path('ListofBadges/getbadge/<int:pk>/',get_badge,name="get_badge"), 
+    path('ListofBadges/delete/<int:pk>/',delete_badge,name="delete_badge"),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
